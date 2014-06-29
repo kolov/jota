efa2e228526b4466ad75336d7eef5b86ff7b961a
Assen Kolov <assen.kolov@gmail.com>
2014-06-29 16:52:56 +0200

    Initial

---
+++ src/milog/core.clj
@@ -0,0 +1,50 @@
+(ns minilo.core)
+
+
+(def logconfig (atom nil))
+
+(defn category [x]
+  "Make a category string"
+  (cond
+    (= (class x) clojure.lang.Namespace) (str (.name x))
+    (= (class x) clojure.lang.Keyword) x
+    :default (str x)))
+
+(defmacro getns [] (eval *ns*))
+
+(defn- set-attr! [x attr val] (swap! logconfig assoc-in [(category x) attr] val))
+(defn set-level! [x level] (set-attr! x :level level))
+(defn set-writer! [x writer] (set-attr! x :writer writer))
+
+(defn- get-attr [x attr]
+  (if-let [found (get-in @logconfig [(category x) attr])]
+    found (get-in @logconfig [:root attr])))
+(defn get-level [x] (get-attr x :level))
+(defn get-writer [x] (get-attr x :writer))
+
+(defn reset-log![] (reset! logconfig {:root {:level :trace :writer println}}))
+(reset-log!)
+
+(defn passes [level configured]
+  (let [order [:trace :debug :info :warn :error]
+        ix-level (.indexOf order level)
+        ix-configured (.indexOf order configured)
+        ]
+    (<= ix-configured ix-level))
+  )
+
+(defn log? [x level] (passes level (get-level x)))
+
+(defn logprint [x level txt]
+  (let [cat (category x)]
+    (if (log? cat level)
+      ((get-writer cat) (str cat ":" (name level) ": " txt)))))
+
+(defmacro dolog [level & args]
+  `(logprint (getns) ~level (apply str (vector ~@args))))
+
+(defmacro trace [& args] `(dolog :trace ~@args))
+(defmacro debug [& args] `(dolog :debug ~@args))
+(defmacro info [& args] `(dolog :info ~@args))
+(defmacro warn [& args] `(dolog :warn ~@args))
+(defmacro error [& args] `(dolog :error ~@args))
---
+++ test/milog/core_test.clj
@@ -0,0 +1,63 @@
+(ns minilo.core-test
+  (:require [clojure.test :refer :all]
+            [minilo.core :refer :all]
+            [midje.sweet :refer :all]))
+
+(fact
+  (category *ns*) => "milog.core-test"
+  (category 'ala.bala) => "ala.bala"
+  (category :ala.bala) => :ala.bala
+  )
+
+(fact
+  (passes :info :info) => true
+  (passes :trace :info) => false
+  (passes :error :info) => true
+  )
+
+(fact
+  "Initial configuration contains root level, writer"
+  (let [dummy (reset-log!)]
+    (keys @logconfig) => [:root]
+    (keys (:root @logconfig)) => [:level :writer]
+    ))
+
+(fact
+  "Setting level en writers ok"
+  (let [dummy (reset-log!)
+        some-fn1 (fn [x] 1)
+        some-fn2 (fn [x] 2)]
+    (do (set-level! "x" :error) (get-level "x")) => :error
+    (do (set-level! "x" :trace) (get-level "x")) => :trace
+    (do (set-writer! "x" some-fn1) (get-writer "x")) => some-fn1
+    (do (set-writer! "x" some-fn2) (get-writer "x")) => some-fn2
+    (do (set-writer! "x" some-fn1) ((get-writer "x") 0)) => 1
+    ))
+
+(fact
+  "levels and writer honoured"
+  (let [dummy (reset-log!)
+        dummy (set-level! :root :debug)
+        dummy (set-writer! :root (fn [x] (print (str "r:" x))))
+        dummy (set-level! "x" :info)
+        dummy (set-writer! "x" (fn [x] (print (str "x:" x))))
+        dummy (set-level! "y" :warn)
+        dummy (set-writer! "y" (fn [x] (print (str "y:" x))))
+        ]
+
+    (with-out-str (logprint "undefined" :trace "11")) => ""
+    (with-out-str (logprint "undefined" :debug "11")) => "r:undefined:debug: 11"
+
+    (with-out-str (logprint "x" :trace "11")) => ""
+    (with-out-str (logprint "x" :info "11")) => "x:x:info: 11"
+    (with-out-str (logprint "x" :warn "11")) => "x:x:warn: 11"
+    (with-out-str (logprint "x" :error "11")) => "x:x:error: 11"
+
+    (with-out-str (logprint "y" :trace "11")) => ""
+    (with-out-str (logprint "y" :info "11")) => ""
+    (with-out-str (logprint "y" :warn "11")) => "y:y:warn: 11"
+    (with-out-str (logprint "y" :error "11")) => "y:y:error: 11"
+
+    ))
+
+
