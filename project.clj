c7c93931c915e4639c961f58ea6c92d6c5820b18
Assen Kolov <assen.kolov@gmail.com>
2015-05-27 17:29:04 +0200

    lein-release plugin: preparing 0.7.6 release

--- project.clj
+++ project.clj
@@ -1,22 +1,23 @@
-(defproject jota "0.6.0"
-            :description "Light logging for clojure"
-            :url "http://github.com/kolov/jota"
-            :license {:name "Eclipse Public License"
-                      :url  "http://www.eclipse.org/legal/epl-v10.html"}
-            :dependencies [[org.clojure/clojure "1.6.0"]]
-            :profiles {
-                       :dev {:dependencies [[midje "1.6.3"]
-                                            [midje-junit-formatter "0.1.0-SNAPSHOT"]]
-                             :plugins      [[lein-deps-tree "0.1.2"]
-                                            [lein-midje "3.0.0"]
-                                            [test2junit "1.0.1"]
-                                            [lein-release "1.0.5"]]
-                             }
-                       }
-            :repositories [
-                           ["snapshots" "http://nexus.akolov.com/content/repositories/snapshots"]
-                           ["releases" { 
-                                 :url "http://nexus.akolov.com/content/repositories/releases"
-                                 :sign-releases false }
-                           ]]
-            )
+(defproject jota "0.7.6"
+  :description "Light logging for clojure"
+  :url "http://github.com/kolov/jota"
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {
+             :dev {:dependencies [[midje "1.6.3"]
+                                  [midje-junit-formatter "0.1.0-SNAPSHOT"]]
+                   :plugins      [[lein-deps-tree "0.1.2"]
+                                  [lein-midje "3.0.0"]
+                                  [test2junit "1.0.1"]
+                                  [lein-release "1.0.5"]]
+                   }
+             }
+  :repositories [
+                 ["snapshots" "http://nexus.akolov.com/content/repositories/snapshots"]
+                 ["releases" {
+                              :url           "http://nexus.akolov.com/content/repositories/releases"
+                              :sign-releases false
+                              }
+                  ]]
+  )
